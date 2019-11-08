844735def7829fcf2a72ae8154c858df538767b3
Korey <54279867+kocheltree@users.noreply.github.com>
2019-11-07 22:36:28 -0700

    Create TitleExtract.py

---
+++ TitleExtract.py
@@ -0,0 +1,22 @@
+# Import Modules
+import json
+import urllib2
+import subprocess
+
+# Collect and parse Icecast JSON array
+data_file = urllib2.urlopen('http://radio.distorted.io:8000/status-json.xsl')
+parsed_json = json.load(data_file)
+title = json.dumps(parsed_json["icestats"]["source"]["title"], indent=4)
+listeners = json.dumps(parsed_json["icestats"]["source"]["listeners"], indent=4)
+
+title = title.replace('"','')
+
+t = open('title.txt','w')
+t.write(title)
+t.close()
+
+l = open('listeners.txt','w')
+l.write(listeners)
+l.close()
+
+# http://distorted.io/
